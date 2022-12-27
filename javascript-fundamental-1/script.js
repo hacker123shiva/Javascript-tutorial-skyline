8b152cc8c3fd9aafeb275400ab359e71be06df1f
Shiva srivastava <105240067+hacker123shiva@users.noreply.github.com>
2022-12-28 01:09:37 +0530

    first commit

---
+++ javascript-fundamental-1/script.js
@@ -0,0 +1,25 @@
+//  console.log("i am a javascript trainer");
+//  console.log("i am a javascript trainer");
+
+let x=5;
+let y=5.6;
+let age=22;
+let name="sachin";
+let isMarried=true;
+
+//let --- keyword to declare a variable
+//x,y,age,name,isMarried --- variable name
+//5,5.6,22,"sachin",true --- value
+
+console.log(x);
+console.log(y);
+console.log(age);
+console.log(name);
+console.log(isMarried);
+
+console.log(5);
+console.log(5.6);
+console.log(22);
+console.log("sachin");
+console.log(true);
+
